c671f050e6a0243b1bf4543c79a8399e2a7f6ade
TOMAS SPIT <Tomas.p.spit1@gmail.com>
2023-06-28 14:15:17 +0200

    Creation of the entities

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -10,7 +10,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 use Gedmo\Timestampable\Traits\TimestampableEntity;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
+#[UniqueEntity(fields: ['email'], message: 'Cette email existe déjà. Merci de choisir une autre.')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     use TimestampableEntity;
